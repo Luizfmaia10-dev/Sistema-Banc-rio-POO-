83ee00c8187e2635702f65a8789cf68e12ea32c2
LuizMaia <lf.cunhamaia@gmail.com>
2026-02-21 01:02:35 -0300

    fix:Mudança no Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,17 @@
 package main;
 
 import service.Banco;
+import server.ServidorHttp;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
         Banco banco = new Banco();
-        Scanner sc = new Scanner(System.in);
+        ServidorHttp.iniciar(banco);
 
+        Scanner sc = new Scanner(System.in);
         int opcao;
 
         do{
@@ -23,25 +25,18 @@ public class Main {
             opcao = sc.nextInt();
 
             switch(opcao){
-
                 case 1:
                     break;
-
                 case 2:
                     break;
-
                 case 3:
                     break;
-
                 case 4:
                     break;
-
                 case 5:
                     break;
-
             }
 
         }while(opcao != 0);
-
     }
 }
